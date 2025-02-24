2c30ca283bf57edc03142c3b391b3065374bc4f6
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-24 12:40:45 +0500

    OOP - Class/Object

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
 public class App {
     public static void main(String args[]) {
 
+        int num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, even = 0, odd = 1;
+
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even Number = " + even);
+        System.out.println("Odd Number = " + odd);
+
+        System.out.println("<<<<<<===================>>>>>>>>>>>");
+
         // Data Class Object
         StudentData studentData = new StudentData();
         studentData.studenName = "Waqas Muhammad";
@@ -10,6 +25,8 @@ public class App {
         System.out.println("Student Name: " + studentData.studenName);
         System.out.println("Student Father Name: " + studentData.studentFatherName);
         System.out.println("Student Age: " + studentData.studentAge);
+
+        System.out.println("<<<<<<===================>>>>>>>>>>>");
 
         // Employee Class Object
         Employee employee = new Employee();
@@ -21,9 +38,13 @@ public class App {
         System.out.println("Employee Age: " + employee.employeeAge);
         System.out.println("Employee Salary: " + employee.employeeSalary);
 
+        System.out.println("<<<<<<===================>>>>>>>>>>>");
+
         // Mobile Phone Object
         MobilePhone mobilePhone = new MobilePhone();
         mobilePhone.mobileInfo("Samsung Galaxy Note 8", "SM-905");
+
+        System.out.println("<<<<<<===================>>>>>>>>>>>");
     }
 }
 

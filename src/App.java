414f64b219285a707b08fde77dfd10869678ff8f
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-24 12:10:04 +0500

    OOP - Class/Object

--- src/App.java
+++ src/App.java
@@ -1,17 +1,15 @@
 public class App {
     public static void main(String args[]) {
 
-
-        
         // Data Class Object
-        Data data = new Data();
-        data.studenName = "Waqas Muhammad";
-        data.studentFatherName = "Muhammad Haseeb";
-        data.studentAge = 18;
+        StudentData studentData = new StudentData();
+        studentData.studenName = "Waqas Muhammad";
+        studentData.studentFatherName = "Muhammad Haseeb";
+        studentData.studentAge = 18;
 
-        System.out.println("Student Name: " + data.studenName);
-        System.out.println("Student Father Name: " + data.studentFatherName);
-        System.out.println("Student Age: " + data.studentAge);
+        System.out.println("Student Name: " + studentData.studenName);
+        System.out.println("Student Father Name: " + studentData.studentFatherName);
+        System.out.println("Student Age: " + studentData.studentAge);
 
         // Employee Class Object
         Employee employee = new Employee();
@@ -22,18 +20,39 @@ public class App {
         System.out.println("Employee Name: " + employee.employeeName);
         System.out.println("Employee Age: " + employee.employeeAge);
         System.out.println("Employee Salary: " + employee.employeeSalary);
+
+        // Mobile Phone Object
+        MobilePhone mobilePhone = new MobilePhone();
+        mobilePhone.mobileInfo("Samsung Galaxy Note 8", "SM-905");
     }
 }
 
-class Data {
+// Student Data Class
+class StudentData {
     String studenName;
     String studentFatherName;
     int studentAge;
 }
 
+// Employee Class
 class Employee {
     String employeeName;
     int employeeAge;
     int employeeSalary;
+
+}
+
+// Mobile Class
+class MobilePhone {
+    String mobileName;
+    String mobileModel;
+
+    void mobileInfo(String mobName, String mobModel) {
+        this.mobileName = mobName;
+        this.mobileModel = mobModel;
+
+        System.out.println("Mobile Name: " + mobName);
+        System.out.println("Mobile Model: " + mobModel);
+    }
 
 }
